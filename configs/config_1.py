96aeac330ed3b486783ca3369b0e242b5fe41f96
Maxim <maximabbas@gmx.de>
2026-06-15 13:35:33 +0200

    Add initial TabTune experiment pipeline

--- configs/config_1.py
+++ configs/config_1.py
@@ -3,10 +3,10 @@ from pathlib import Path
 config_base = {}
 
 config_base["model"] = "tabpfn_v3"
-config_base["open_ml_task_id"] = 13 
+config_base["open_ml_task_id"] = 53
 config_base["finetuning_method"] = "no_finetuning" #"full_finetuning" 
 
 
 #config_base["benchmarks"] = "tabarena" # 
 
-config_base["saving_path"] = "../results/finetuning_experiments/" + config_base["model"] + "/" + config_base["finetuning_method"] + "/"
+config_base["saving_path"] = "../results/finetuning_experiments/" + config_base["model"] 
